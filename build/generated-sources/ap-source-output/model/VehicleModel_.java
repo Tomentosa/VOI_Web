c4b3e15b246eb5b10836dc1e53284841baf233a6
Brian Avadikian <Bavadikian@gmail.com>
2022-03-04 15:29:16 -0500

    Completed UI layout for the main page - need to attach functionality

--- build/generated-sources/ap-source-output/model/VehicleModel_.java
+++ build/generated-sources/ap-source-output/model/VehicleModel_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.VehicleMake;
 import model.VehicleOfInterest;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-03-04T08:41:55")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-03-04T13:41:56")
 @StaticMetamodel(VehicleModel.class)
 public class VehicleModel_ { 
 
--- build/generated-sources/ap-source-output/model/VehicleOfInterest_.java
+++ build/generated-sources/ap-source-output/model/VehicleOfInterest_.java
@@ -7,7 +7,7 @@ import model.ReasonForInterest;
 import model.VehicleMake;
 import model.VehicleModel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2022-03-04T08:41:55")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-03-04T13:41:56")
 @StaticMetamodel(VehicleOfInterest.class)
 public class VehicleOfInterest_ { 
 
