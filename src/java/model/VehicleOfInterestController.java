1d8cabe8b037366b26b6c038d0b21d9bb767e6bb
Brian Avadikian <Bavadikian@gmail.com>
2022-03-06 20:11:49 -0500

    Sucessfully refactored table, working on pagination overhaul next

--- src/java/model/VehicleOfInterestController.java
+++ src/java/model/VehicleOfInterestController.java
@@ -46,7 +46,7 @@ public class VehicleOfInterestController implements Serializable {
 
     public PaginationHelper getPagination() {
         if (pagination == null) {
-            pagination = new PaginationHelper(10) {
+            pagination = new PaginationHelper(20) {
 
                 @Override
                 public int getItemsCount() {
